b76f9864b56e2f6d4a905b5c88acc6365a644ada
hamza <hamza@jsfactory.co>
2024-08-17 19:37:38 +0700

    Automatic commit from Bubble

---
+++ elements/AAC-850m6/preview.js
@@ -0,0 +1,12 @@
+function(instance, properties) {
+	
+   
+    var src = '//meta-q.cdn.bubble.io/f1723897258742x704797899090876500/Screen%20Shot%202024-08-17%20at%2019.20.23.png'
+    
+    var img = document.createElement('img');
+    img.setAttribute('src', src);
+    img.setAttribute('width', '100%');
+    
+    instance.canvas.append(img);
+
+}
